aa5c3395b699ab0f25e6a1eaec697a98060ca703
Brandon George <brandongeorge0920@gmail.com>
2022-11-15 16:10:48 -0500

    final project

--- MedChart (1)/MedChart/src/main/java/net/codejava/MedChart/AppContoller/AppController.java
+++ MedChart (1)/MedChart/src/main/java/net/codejava/MedChart/AppContoller/AppController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.configurationprocessor.json.JSONArray;
 import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
@@ -18,6 +19,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import com.kwabenaberko.newsapilib.NewsApiClient;
+import com.kwabenaberko.newsapilib.models.Source;
+import com.kwabenaberko.newsapilib.models.request.TopHeadlinesRequest;
+import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
+
 
 /**
  *
@@ -49,10 +55,57 @@ public class AppController {
     }
     
     @GetMapping("/admin/quote")
-    public List<Object> getNews() throws JSONException {
+    public Object getNews() throws JSONException {
+
+        NewsApiClient newsApiClient = new NewsApiClient("26fcd79cfdd14681971663b9faa0cd9f");
+
+        // /v2/top-headlines
+newsApiClient.getTopHeadlines(
+    new TopHeadlinesRequest.Builder()
+      .q("bitcoin")
+      .language("en")
+      .build(),
+    new NewsApiClient.ArticlesResponseCallback() {
+      @Override
+      public void onSuccess(ArticleResponse response) {
+        System.out.println(response.getArticles().get(0).getTitle());
+      }
+  
+      @Override
+      public void onFailure(Throwable throwable) {
+        System.out.println(throwable.getMessage());
+      }
+    }
+  );
+
+  String aurl = "https://newsapi.org/v2/top-headlines?q=health&country=us&category=health&apiKey=26fcd79cfdd14681971663b9faa0cd9f";
+  RestTemplate restTemplate = new RestTemplate();
+  Object jSonQuote = restTemplate.getForObject(aurl, Object.class);
+
+  //Print the whole response to console.
+  String quote = restTemplate.getForObject(aurl, String.class);
+  //Parse out the most important info from the response.
+  JSONObject jo = new JSONObject(quote);
+  JSONObject ja = jo.optJSONObject("articles");
+  System.out.println(jo.toString());
+  
+  for(int i = 0; i < jo.getJSONArray("articles").length(); i++){
+    String author = jo.getString("author");
+        String title = jo.getString("title");
+        String description = jo.getString("description");
+        String url = jo.getString("url");
+        String urlToImage = jo.getString("urlToImage");
+        String publishedAt = jo.getString("publishedAt");
+        String content = jo.getString("content");
+
+        System.out.println(author + "" + title + "" + description 
+        + "" + url + "" +  urlToImage + "" + publishedAt + "" + content);
+  }
+
+  return jSonQuote;
     
         
-        String apiUrl = "https://newsapi.org/v2/top-headlines?q=health&country=us&category=health&apiKey=26fcd79cfdd14681971663b9faa0cd9f";
+        /*String apiUrl = "https://newsapi.org/v2/top-headlines?q=health&country=us&category=health&apiKey=26fcd79cfdd14681971663b9faa0cd9f";
         RestTemplate restTemplate = new RestTemplate();
         Object[] HealthNews = restTemplate.getForObject(apiUrl, Object[].class);
         
@@ -62,7 +115,7 @@ public class AppController {
         JSONArray news = new JSONArray(HealthNews);
         System.out.println(news.toString());
         
-        /*Iterator<Object> test = news.iterator();
+        Iterator<Object> test = news.iterator();
 
         while (test.hasNext()){
             JSONObject jo = (JSONObject) test.next();
@@ -81,7 +134,7 @@ public class AppController {
     
         
 
-        return Arrays.asList(HealthNews);
+        //return Arrays.asList(HealthNews);
     }
     
     @GetMapping("/medStaff/Home") 
