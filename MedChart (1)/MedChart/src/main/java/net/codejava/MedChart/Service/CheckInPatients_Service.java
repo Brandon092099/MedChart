7e4d04bd32f0414b27f6186525fcc0bfa8349346
David <amayadavid885@gmail.com>
2022-11-15 14:24:16 -0500

    update 2

--- MedChart (1)/MedChart/src/main/java/net/codejava/MedChart/Service/CheckInPatients_Service.java
+++ MedChart (1)/MedChart/src/main/java/net/codejava/MedChart/Service/CheckInPatients_Service.java
@@ -1,6 +1,3 @@
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
 package net.codejava.MedChart.Service;
 import java.util.List;
 import net.codejava.MedChart.User.CheckInPatients;
@@ -12,20 +9,3 @@ public interface CheckInPatients_Service {
     CheckInPatients getCheckInPatientsById(long id);
     
 }
-=======
->>>>>>> Stashed changes
-package net.codejava.MedChart.Service;
-import java.util.List;
-import net.codejava.MedChart.User.CheckInPatients;
-public interface CheckInPatients_Service {
-    List<CheckInPatients> getAllCheckInPatients();
-    
-    void savePatients(CheckInPatients CheckInPatients);
-    
-    CheckInPatients getCheckInPatientsById(long id);
-    
-}
-<<<<<<< Updated upstream
-=======
->>>>>>> 0791adb155f182536dc412997e8efe8d13522e0b
->>>>>>> Stashed changes
--- MedChart (1)/MedChart/src/main/java/net/codejava/MedChart/Service/CheckInPatients_ServiceImpl.java
+++ MedChart (1)/MedChart/src/main/java/net/codejava/MedChart/Service/CheckInPatients_ServiceImpl.java
@@ -1,6 +1,3 @@
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
 package net.codejava.MedChart.Service;
 import java.util.List;
 import java.util.Optional;
@@ -36,44 +33,3 @@ public class CheckInPatients_ServiceImpl implements CheckInPatients_Service{
         return patient;
     }
 }
-=======
->>>>>>> Stashed changes
-package net.codejava.MedChart.Service;
-import net.codejava.MedChart.Repository.CheckInPatients_Repository;
-import java.util.List;
-import java.util.Optional;
-import net.codejava.MedChart.User.CheckInPatients;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class CheckInPatients_ServiceImpl implements CheckInPatients_Service{
-    @Autowired
-    private CheckInPatients_Repository CheckInPatientsRepo;
-    
-    @Override
-    public List<CheckInPatients> getAllCheckInPatients() {
-        return CheckInPatientsRepo.findAll();
-    }
-
-    @Override
-    public void savePatients(CheckInPatients patients) {
-        this.CheckInPatientsRepo.save(patients);
-    }
-    
-    @Override
-    public CheckInPatients getCheckInPatientsById(long id) {
-        Optional<CheckInPatients> optional = CheckInPatientsRepo.findById(id);
-        CheckInPatients patient = null;
-        if (optional.isPresent()) {
-            patient = optional.get();
-        } else {
-            throw new RuntimeException("Patient not found for id:: " + id);
-        }
-        return patient;
-    }
-}
-<<<<<<< Updated upstream
-=======
->>>>>>> 0791adb155f182536dc412997e8efe8d13522e0b
->>>>>>> Stashed changes
